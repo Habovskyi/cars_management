8792723e7895f5dbec07bf174575bf57daef72f5
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2021-11-15 22:58:13 +0200

    update loading file

--- lib/db_reader.rb
+++ lib/db_reader.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
+
 class Database
   FILEPATH = File.dirname(__FILE__)
   NAME_DB = 'db.yml'
 
   def read_database(name_db = NAME_DB)
-    YAML.safe_load(File.read("#{FILEPATH}/../db/#{name_db}"))
+    YAML.load_file("#{FILEPATH}/../db/#{name_db}")
   rescue Errno::ENOENT
     abort 'No car db found, make sure the file exists or check the path to this file.'
   end
