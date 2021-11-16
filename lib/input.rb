d8fef137a076b1d1e6d3ec3ef26b55c361f62afb
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2021-11-16 15:04:51 +0200

    change unused arguments and  replace string to symbol

--- lib/input.rb
+++ lib/input.rb
@@ -2,20 +2,20 @@
 
 class Input
   def initialize
-    @user_rules = { 'make': ' ',
-                    'model': ' ',
-                    'year_from': 0,
-                    'year_to': 0,
-                    'price_from': 0,
-                    'price_to': 0 }
+    @user_rules = { make: '',
+                    model: '',
+                    year_from: 0,
+                    year_to: 0,
+                    price_from: 0,
+                    price_to: 0 }
   end
 
   def call
     puts 'Please select search rules.'
-    user_rules.each do |key, value|
+    user_rules.each do |key, _value|
       puts "Please choose #{key}: "
       user_rules[key] = gets.chomp
-      user_rules.delete_if { |key, value| value == '' }
+      user_rules.delete_if { |_key, value| value == '' }
     end
     user_rules
   end
