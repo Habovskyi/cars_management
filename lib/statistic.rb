5b25e126557d5b6d009c0e4764441094a7eae4c0
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2021-11-25 19:00:45 +0200

    refactoring methods for count statistics

--- lib/statistic.rb
+++ lib/statistic.rb
@@ -3,22 +3,17 @@
 # Class for calculating statistics
 class Statistic
   DB_NAME = 'searches.yml'
-  DEFAULT_REQUEST_COUNTER = 1
 
   def initialize(user_rules, result_searching)
     @user_rules = user_rules
     @result_searching = result_searching
     @db = Database.new(DB_NAME)
+    @statistic = { search: user_rules, statistic: { total_quantity: total_counter, requests_quantity: 1} }
   end
 
   def call
-    if load_statistic == false
-      @statistic = [{ search: user_rules, statistic: create_statistic }]
-      @db.write(@statistic)
-    else
-      unique_record
-    end
-    create_statistic
+    load_statistic == false ? @db.write([@statistic]) : unique_record
+    @statistic
   end
 
   private
@@ -30,36 +25,20 @@ class Statistic
   def unique_record
     @data.each do |request|
       if request[:search] == user_rules
-        request[:statistic][:requests_quantity] = request_counter
+        request[:statistic][:requests_quantity] += 1
         @check = true
       end
     end
     add_new_record unless @check
-    write_statistic
+    @db.write(@data)
   end
 
   def add_new_record
-    @statistic = { search: @user_rules, statistic: create_statistic }
     @data << @statistic
-  end
-
-  def create_statistic
-    @quantity = { total_quantity: total_counter, requests_quantity: @count || 1 }
-  end
-
-  def write_statistic
-    @db.write(@data)
   end
 
   def total_counter
     result_searching.length
-  end
-
-  def request_counter
-    @data.each do |request|
-      @count = request.dig(:statistic, :requests_quantity) + 1 if request[:search] == user_rules
-    end
-    @count
   end
 
   attr_reader :user_rules, :result_searching
