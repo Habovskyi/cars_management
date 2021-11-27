e27dbe0b18cace40b513fb4355c81894d8c93b74
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2021-11-27 21:06:53 +0200

    change validate method

--- lib/data_updater.rb
+++ lib/data_updater.rb
@@ -15,8 +15,8 @@ class DataUpdater
 
   def validate_number
     change_max_value
-    user_rules[:year_to] = @max_year if user_rules[:year_to].to_i.zero?
-    user_rules[:price_to] = @max_price if user_rules[:price_to].to_i.zero?
+    user_rules[:year_to] = @max_year if user_rules[:year_to].to_i <= 0
+    user_rules[:price_to] = @max_price if user_rules[:price_to].to_i <= 0
     user_rules.delete_if { |_key, value| value.to_s.empty? }
     user_rules
   end
