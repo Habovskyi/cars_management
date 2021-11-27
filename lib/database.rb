9ba29293ea696d70373563741c3e3c84da9a5348
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2021-11-27 20:57:24 +0200

    add fix to internationalization

--- lib/database.rb
+++ lib/database.rb
@@ -6,6 +6,7 @@ class Database
   NAME_DB = 'db.yml'
 
   def initialize(name_db = NAME_DB)
+    @name_db = name_db
     @path = "#{FILEPATH}/../db/#{name_db}"
     create_file unless File.file?(path)
     @db = File.open(path, 'r+')
@@ -24,7 +25,8 @@ class Database
 
   def create_file
     File.new(path, 'w')
+    puts I18n.t('message_create_database', name_db: name_db)
   end
 
-  attr_reader :db, :path
+  attr_reader :db, :path, :name_db
 end
