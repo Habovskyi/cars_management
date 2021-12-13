b078e306529096d79723887d75ac2cfca3041338
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2021-12-14 01:22:24 +0200

    delete return value

--- lib/console/console.rb
+++ lib/console/console.rb
@@ -18,7 +18,6 @@ module Lib
       def input_user_rules
         print_text('input.search.select')
         @user_rules.keys.map { |key| @user_rules[key] = input("input.search.rules.#{key}").capitalize }
-        @user_rules
       end
 
       def input_sort_rules
